86f6e56dbaf5967bc4b10b543c588d15ea8fd5b6
David Freerksen <dfreerksen@gmail.com>
2015-02-27 16:32:15 -0800

    WIP: Allow user to select part variants when adding a bundle to the cart
    ---
    
    When a product bundle is added to the cart, the bundle is the
    only LineItem, therefore, it looks like we'll need to store variant
    selection information as an attribute on LineItem, necessitating a new
    column or an associated table.
    
    From there, we'll probably also need to make sure we're consistent with
    several things:
    
    - inventory level checks
    - pricing
    - showing the selected variant information on the front-end
    - shipping the correct variant(s)
    - reports (probably will not auto-magically contain variant selection info)
    
    - Trello card: https://trello.com/c/1asokxrA

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -67,6 +67,63 @@ describe "Checkout", type: :feature do
 
   end
 
+  context "when a part allows User to select any variant" do
+    xit "does not show parts that are out-of-stock" do
+    end
+
+    xit "does not allow selection of variants that are out-of-stock" do
+
+    end
+
+    it "shows the part the User selected at all stages of checkout" do
+      bundle = create(:product)
+      red_option = create(:option_value, presentation: "Red")
+      blue_option = create(:option_value, presentation: "Blue")
+
+      option_type = create(:option_type, presentation: "Color", name: "color")
+      option_type.option_values = [red_option, blue_option]
+
+      part = create(:product, option_types: [option_type], can_be_part: true)
+
+      create(:variant, product: part) do |variant|
+        variant.option_values = [red_option]
+      end
+      create(:variant, product: part) do |variant|
+        variant.option_values = [blue_option]
+      end
+
+      create(:assemblies_part,
+        assembly: bundle,
+        part_id: part.master.id,
+        variant_selection_deferred: true
+      )
+
+      visit spree.root_path
+      click_link bundle.name
+
+      select "Blue", from: "Color"
+      click_button "add-to-cart-button"
+
+      click_button "Checkout"
+
+      fill_in "order_email", :with => "ryan@spreecommerce.com"
+      fill_in_address
+
+      click_button "Save and Continue"
+      expect(current_path).to eql(spree.checkout_state_path("delivery"))
+      page.should have_content(variant.product.name)
+      page.should have_content("Blue")
+
+      click_button "Save and Continue"
+      expect(current_path).to eql(spree.checkout_state_path("payment"))
+      page.should have_content("Blue")
+
+      click_button "Save and Continue"
+      expect(current_path).to eql(spree.order_path(Spree::Order.last))
+      page.should have_content(variant.product.name)
+      page.should have_content("Blue")
+    end
+  end
 
   def fill_in_address
     address = "order_bill_address_attributes"
