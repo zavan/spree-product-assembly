fa832499b79c3dd18b1e6b649a89963d7bc1e1e8
KacperMekarski <kacper.mekarski@sparksolutions.co>
2021-01-28 13:47:01 +0100

    Reject bundles from unshipabble products list

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,6 +4,14 @@ module Spree
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
       def before_payment
+        if @order.checkout_steps.include? 'delivery'
+          packages = @order.shipments.map(&:to_package)
+          @differentiator = Spree::Stock::Differentiator.new(@order, packages)
+          @differentiator.missing.reject{ |variant| variant.try(:parts).try(:any?) }.each do |variant, quantity|
+            Spree::Dependencies.cart_remove_item_service.constantize.call(order: @order, variant: variant, quantity: quantity)
+          end
+        end
+
         @payment_sources = try_spree_current_user.payment_sources if try_spree_current_user&.respond_to?(:payment_sources)
       end
     end
