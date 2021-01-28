3bd2ca73209ccfce9afbcde8e5902e8aad87236d
KacperMekarski <kacper.mekarski@sparksolutions.co>
2021-01-28 11:27:26 +0100

    Add payment sources class variable

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,7 +3,9 @@ module Spree
     module CheckoutControllerDecorator
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
-      def before_payment; end
+      def before_payment
+        @payment_sources = try_spree_current_user.payment_sources if try_spree_current_user&.respond_to?(:payment_sources)
+      end
     end
   end
 end
